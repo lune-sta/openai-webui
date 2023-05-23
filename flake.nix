352abc5b0c35999e07ab2e083b6c1c35dd5744ff
fujigon <dec12.kaz@gmail.com>
2023-05-23 16:01:58 +0900

    env: a reproducible env environment based on nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake utils demo";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells = rec {
+            default = pkgs.mkShell {
+                buildInputs = [pkgs.poetry];
+            };
+        };
+      }
+    );
+}
